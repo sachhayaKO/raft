58f40358c50eb5d0d972fd1dfaa27ea625ca74c3
Your Name <sachhaya1@gmail.com>
2026-06-04 19:28:37 -0400

    Update devlog + build consensus module struct

--- DEVLOG.md
+++ DEVLOG.md
@@ -12,3 +12,10 @@
 - Done: created basic file layout (raft/types.go, raft/consensus.go, raft/server.go), initialized go module, defined project data types
 - Blocked on: none
 - Next: finish project timeline and begin consensus logic
+
+## 2026-06-04 - Sachin 
+- Working on: Consensus struct creation 
+- Learning typing and struct creation
+- Next: begin coding consensus logic
+- Important concepts learned: Each module has a perisistant state, volatile state, and leaders only have another volatile state. 
+- Next Steps: building the NewConsensusModule constructorr. 
--- raft/consensus.go
+++ raft/consensus.go
@@ -1,1 +1,47 @@
 package raft
+
+import "sync"
+
+type ConsensusModule struct {
+	id    int
+	peers []int
+
+	//persistent state
+
+	/*
+		This variable prevents past elections conflicting with present
+		For example, if you restart with term 0, you'll grant votes to candidates
+		you already voted against, and you might accept a leader from a term you already
+		participated in.
+	*/
+	currentTerm int
+
+	// Makes sure that if a node crashes, it remembers who they votes for
+	// Nodes cannot vote for more than 1 candidate
+	votedFor int
+
+	//Log of all the LogEntries
+	log []LogEntry
+
+	//volatile state
+
+	//Highest index where majority of nodes have the entry in their log
+	commitIndex int
+
+	//Highest term entry has been executed against the state machine
+	lastApplied int
+
+	//leader volatile state
+
+	//Index of the next log entry the leader will send to each follower
+	nextIndex []int
+
+	//Highest log index the leader knows has been replicated on a follower
+	matchIndex []int
+
+	//Tracks whether this node is a Follower, Candidate or Leader
+	state CMState
+
+	//Lock for writing to the state
+	mu sync.Mutex
+}
